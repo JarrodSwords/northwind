1b02741937f2e4860945194edfb5baa76e708f4c
Jarrod <jarrod.swords@callibrity.com>
2021-01-18 19:57:08 -0500

    add factory method

--- infrastructure-postgresql/Order.cs
+++ infrastructure-postgresql/Order.cs
@@ -19,5 +19,26 @@ namespace Northwind.Infrastructure.PostgreSql
         public string ship_region { get; set; }
         public string ship_postal_code { get; set; }
         public string ship_country { get; set; }
+
+        public Domain.Order ToDomain()
+        {
+            return new (
+                order_id,
+                customer_id,
+                employee_id,
+                order_date,
+                required_date,
+                shipped_date,
+                ship_via,
+                freight,
+                new Domain.Address(
+                    ship_name,
+                    ship_address,
+                    ship_city,
+                    ship_region,
+                    ship_postal_code,
+                    ship_country)
+            );
+        }
     }
 }
--- infrastructure-postgresql/OrderRepository.cs
+++ infrastructure-postgresql/OrderRepository.cs
@@ -15,27 +15,7 @@ namespace Northwind.Infrastructure.PostgreSql
 
         public Domain.Order Find(int id)
         {
-            var record = _connection.Get<Order>(id);
-
-            var order = new Domain.Order(
-                record.order_id,
-                record.customer_id,
-                record.employee_id,
-                record.order_date,
-                record.required_date,
-                record.shipped_date,
-                record.ship_via,
-                record.freight,
-                new Address(
-                    record.ship_name,
-                    record.ship_address,
-                    record.ship_city,
-                    record.ship_region,
-                    record.ship_postal_code,
-                    record.ship_country)
-            );
-
-            return order;
+            return _connection.Get<Order>(id).ToDomain();
         }
     }
 }
